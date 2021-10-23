db905a57dee3d0630ed248e0ba7ff4855b645641
Danval-003 <77825594+Danval-003@users.noreply.github.com>
2021-10-23 11:58:42 -0600

    Alfabetizate

---
+++ app/src/main/java/com/example/alfabetizate/ALFAinicio.java
@@ -0,0 +1,22 @@
+package com.example.alfabetizate;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ALFAinicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(ALFAinicio.this, TestInicio.class);
+                startActivity(intent);
+            }
+        },4000);
+    }
+}
---
+++ app/src/main/java/com/example/alfabetizate/TestInicio.java
@@ -0,0 +1,13 @@
+package com.example.alfabetizate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_inicio);
+    }
+}
