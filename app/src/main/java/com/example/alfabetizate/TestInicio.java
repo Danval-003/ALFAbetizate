7661e504349a87acfb5e05ecc536274e1bdab633
Danval-003 <77825594+Danval-003@users.noreply.github.com>
2021-10-24 13:12:54 -0600

    MEnu y completacion de la apariencia del test
    Signed-off-by: Danval-003 <77825594+Danval-003@users.noreply.github.com>

--- app/src/main/java/com/example/alfabetizate/TestInicio.java
+++ app/src/main/java/com/example/alfabetizate/TestInicio.java
@@ -1,6 +1,9 @@
 package com.example.alfabetizate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,11 @@ public class TestInicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_inicio);
+        Button j= (Button) findViewById(R.id.Fin);
+        j.setOnClickListener(this::onClick);
+    }
+    public void onClick(View v){
+        Intent i = new Intent(TestInicio.this, menu.class);
+        startActivity(i);
     }
 }
